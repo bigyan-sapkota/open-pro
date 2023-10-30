0ffd10ac594b2d507c6e225574c84256045dd867
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-10-30 22:46:17 +0545

    Working on footer seciton

---
+++ src/Components/FooterNavigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const data = [
+  {
+    category: "Products",
+    items: ["Web Studio", "DynamicBox Flex", "Programming Forms"],
+  },
+  {
+    category: "Resources",
+    items: ["Nostrud exercitation", "Visual mockups"],
+  },
+  {
+    category: "Company",
+    items: ["Consectetur adipiscing", "Labore et dolore"],
+  },
+];
+
+const FooterNavigation = () => {
+  return <div></div>;
+};
+
+export default FooterNavigation;
---
+++ src/Containers/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import FooterNavigation from "../Components/FooterNavigation";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div>logo</div>
+        <p>
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing industries for previewing layouts and visual mockups.
+        </p>
+        <p>© Cruip.com. All rights reserved.</p>
+      </div>
+      <div>
+        <FooterNavigation />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
